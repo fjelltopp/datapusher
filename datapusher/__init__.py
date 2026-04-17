cefdc3b0adf7e1054558225165f754cd6c4bb394
toavina <toavinahajaina@gmail.com>
2026-04-17 12:23:49 +0300

    Narrow monkey patch version check to Python 3.10+

--- datapusher/__init__.py
+++ datapusher/__init__.py
@@ -4,7 +4,7 @@ __version__ = '0.0.18'
 # which was removed in Python 3.10 (moved to collections.abc). Since upgrading
 # messytables is not an option, we restore the old attributes before it imports.
 import sys
-if sys.version_info >= (3, 3):
+if sys.version_info >= (3, 10):
     import collections.abc
     if not hasattr(collections, 'Mapping'):
         collections.Mapping = collections.abc.Mapping
